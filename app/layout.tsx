c2558c23bef385304ee404b6eb526bf8d9220207
fruitbox12 <dylanwong007@gmail.com>
2024-04-11 05:21:04 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
       </body>
